92b87514e23730b40ad785bc5dd2ec1ec780046b
nikang <mohamadnikan2013@gmail.com>
2017-01-26 19:20:23 +0330

    interpolation semi complate

--- Interpolation.py
+++ Interpolation.py
@@ -1,14 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interpolate
+import pandas as pd
+from statsmodels.sandbox.regression import gmm
 
-x = np.linspace(-3, 3, 150)
-# print(x)
-y = np.power(x, 2) + 0.1 * np.random.randn(150)
-# print(y)
-# spl = InterpolatedUnivariateSpline(x, y)
-spl = interpolate.interp1d(x, y)
-plt.plot(x, y, 'ro', ms=5)
-xs = np.linspace(-3, 3, 150000000)
-plt.plot(xs, spl(xs), 'g', lw=3, alpha=0.7)
-plt.show()
+
+class Interpolation():
+    def __init__(self, deg, x, y):
+        self.y = y
+        self.x = x
+        self.deg = deg
+        self.minimum_mean_square_error_coefficients = self.minimum_mean_square_error()
+
+    def minimum_mean_square_error(self):
+        return np.polyfit(self.x, self.y, self.deg)
+
+    def method_of_moment(self):
+        pass
+
+    def show_minimum_mean_square_error(self):
+        xp = np.linspace(0, 3, 10000)
+        p = np.poly1d(self.minimum_mean_square_error_coefficients)
+        plt.plot(self.x, self.y, '.', xp, p(xp), '-')
