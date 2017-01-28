ef67b5de7bbf1e4565543bd27e7c4b8e359e3025
nikang <mohamadnikan2013@gmail.com>
2017-01-28 23:22:43 +0330

    phase 1 complete

--- NGon.py
+++ NGon.py
@@ -1,96 +1,155 @@
-import numpy as np
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import NullFormatter
 from scipy.spatial.qhull import ConvexHull
-from shapely.geometry.polygon import Polygon
 from shapely.geometry.point import Point
+from shapely.geometry.polygon import Polygon
 
 
 class MyPolygon:
-    def __init__(self, sides):
+    def __init__(self, sides=3):
         self.points = None
         self.hull = None
         self.polygon = None
         self.sides = sides
+        self.areas = defaultdict(list)
 
-    # TODO SAVE IMages
-    def generate_convex_hull_unit_square(self):
-        self.points = np.random.rand(3, 2)
-        self.hull = ConvexHull(self.points)
-        self.polygon = Polygon(Polygon(self.points).convex_hull)
-        while len(self.hull.vertices) < self.sides:
-            # print(len(self.hull.vertices))
-            rand = np.random.rand(1, 2)
-            point = Point(rand[0][0], rand[0][1])
-            if not self.polygon.contains(point):
-                # self.hull.add_points(rand)
-                self.points = np.concatenate((self.points, rand))
-                self.hull = ConvexHull(self.points)
-                self.polygon = Polygon(Polygon(self.points).convex_hull)
+    def generate_convex_hull_unit_square(self, samples):
+        for i in range(samples):
+            self.points = np.random.rand(3, 2)
+            self.hull = ConvexHull(self.points)
+            self.polygon = Polygon(Polygon(self.points).convex_hull)
+            while len(self.hull.vertices) < self.sides:
+                rand = np.random.rand(1, 2)
+                point = Point(rand[0][0], rand[0][1])
+                if not self.polygon.contains(point):
+                    self.points = np.concatenate((self.points, rand))
+                    self.hull = ConvexHull(self.points)
+                    self.polygon = Polygon(Polygon(self.points).convex_hull)
 
-    def generate_convex_hull_unit_circle(self):
-        n = 0
-        self.points = np.empty([1, 2])
-        while n < 3:
-            rand = np.random.rand(1, 2)
-            if (rand[0][0] - 0.5) * (rand[0][0] - 0.5) + (rand[0][1] - 0.5) * (rand[0][1] - 0.5) <= .25:
-                self.points = np.concatenate((self.points, rand))
-                n += 1
-        self.hull = ConvexHull(self.points)
-        self.polygon = Polygon(Polygon(self.points).convex_hull)
-        while len(self.hull.vertices) < self.sides:
-            rand = np.random.rand(1, 2)
-            point = Point(rand[0][0], rand[0][1])
-            if not self.polygon.contains(point) and (
-                                (rand[0][0] - 0.5) * (rand[0][0] - 0.5) + (rand[0][1] - 0.5) * (
-                                    rand[0][1] - 0.5) <= .25):
-                # self.hull.add_points(rand)
-                self.points = np.concatenate((self.points, rand))
-                self.hull = ConvexHull(self.points)
-                self.polygon = Polygon(Polygon(self.points).convex_hull)
+            self.areas[self.sides].append(self.polygon.area)
 
-    def generate_convex_hull_unit_equilateral_triangle(self):
-        n = 0
-        self.points = np.empty([1, 2])
-        while n < 3:
-            rand = np.random.rand(1, 2)
-            if (rand[0][0] >= np.math.sqrt(3) / 3 * rand[0][1]) and (
-                        rand[0][0] <= 1 - (np.math.sqrt(3) / 3 * rand[0][1])):
-                self.points = np.concatenate((self.points, rand))
-                n += 1
-        self.hull = ConvexHull(self.points)
-        self.polygon = Polygon(Polygon(self.points).convex_hull)
-        while len(self.hull.vertices) < self.sides:
-            print(len(self.hull.vertices))
-            rand = np.random.rand(1, 2)
-            point = Point(rand[0][0], rand[0][1])
-            if not self.polygon.contains(point) and (
-                        (rand[0][0] >= np.math.sqrt(3) / 3 * rand[0][1]) and (
-                                rand[0][0] <= 1 - (np.math.sqrt(3) / 3 * rand[0][1]))):
-                # self.hull.add_points(rand)
-                self.points = np.concatenate((self.points, rand))
-                self.hull = ConvexHull(self.points)
-                self.polygon = Polygon(Polygon(self.points).convex_hull)
+            # fig, ax = plt.subplots()
+            # ax.add_patch(
+            #     patches.Rectangle(
+            #         (0, 0),  # (x,y)
+            #         1,  # width
+            #         1,  # height
+            #         facecolor="Black"
+            #     )
+            # )
+            # plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
+            # for simplex in self.hull.simplices:
+            #     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'white')
+            # plt.show()
+
+    def generate_convex_hull_unit_circle(self, samples):
+        for i in range(samples):
+            n = 0
+            self.points = np.empty([1, 2])
+            while n < 3:
+                rand = np.random.rand(1, 2)
+                if (rand[0][0] - 0.5) * (rand[0][0] - 0.5) + (rand[0][1] - 0.5) * (rand[0][1] - 0.5) <= .25:
+                    self.points = np.concatenate((self.points, rand))
+                    n += 1
+            self.points = np.delete(self.points, 0, 0)
+            self.hull = ConvexHull(self.points)
+            self.polygon = Polygon(Polygon(self.points).convex_hull)
+            while len(self.hull.vertices) < self.sides:
+                rand = np.random.rand(1, 2)
+                point = Point(rand[0][0], rand[0][1])
+                if not self.polygon.contains(point) and (
+                                    (rand[0][0] - 0.5) * (rand[0][0] - 0.5) + (rand[0][1] - 0.5) * (
+                                        rand[0][1] - 0.5) <= .25):
+                    self.points = np.concatenate((self.points, rand))
+                    self.hull = ConvexHull(self.points)
+                    self.polygon = Polygon(Polygon(self.points).convex_hull)
+
+            self.areas[self.sides].append(self.polygon.area)
+
+            # fig, ax = plt.subplots()
+            # circle = plt.Circle((0.5, 0.5), .5, color="black")
+            # ax.add_artist(circle)
+            # plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
+            # for simplex in p1.hull.simplices:
+            #     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'white')
+            # plt.show()
+
+    def generate_convex_hull_unit_equilateral_triangle(self, samples):
+        for i in range(samples):
+            n = 0
+            self.points = np.empty([1, 2])
+            while n < 3:
+                rand = np.random.rand(1, 2)
+                if (rand[0][0] >= np.math.sqrt(3) / 3 * rand[0][1]) and (
+                            rand[0][0] <= 1 - (np.math.sqrt(3) / 3 * rand[0][1])):
+                    self.points = np.concatenate((self.points, rand))
+                    n += 1
+
+            self.points = np.delete(self.points, 0, 0)
+            self.hull = ConvexHull(self.points)
+            self.polygon = Polygon(Polygon(self.points).convex_hull)
+            while len(self.hull.vertices) < self.sides:
+                rand = np.random.rand(1, 2)
+                point = Point(rand[0][0], rand[0][1])
+                if not self.polygon.contains(point) and (
+                            (rand[0][0] >= np.math.sqrt(3) / 3 * rand[0][1]) and (
+                                    rand[0][0] <= 1 - (np.math.sqrt(3) / 3 * rand[0][1]))):
+                    self.points = np.concatenate((self.points, rand))
+                    self.hull = ConvexHull(self.points)
+                    self.polygon = Polygon(Polygon(self.points).convex_hull)
+
+            self.areas[self.sides].append(self.polygon.area)
+            # fig, ax = plt.subplots()
+            # ax.add_patch(
+            #     patches.RegularPolygon(
+            #         (0.5, np.sqrt(3) / 6),
+            #         3,
+            #         np.sqrt(3) / 3,
+            #         facecolor="Black"
+            #     )
+            # )
+            # plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
+            # for simplex in self.hull.simplices:
+            #     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'white')
+            # plt.show()
 
 
-p1 = MyPolygon(40)
-p1.generate_convex_hull_unit_equilateral_triangle()
-print(len(p1.points))
-# p1 = MyPolygon(100)
-# # p2 = MyPolygon(10)
-# # p1.generate_convex_hull_unit_equilateral_triangle()
-# p1.generate_convex_hull_unit_circle()
-# # p3 = p1.subscription(p2)
-# fig, ax = plt.subplots()
-# circle = plt.Circle((0.5, 0.5), .5, color="black")
-# ax.add_artist(circle)
-#
-# plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
-# for simplex in p1.hull.simplices:
-#     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'white')
-# plt.show()
-# # plt.plot(p2.points[:, 0], p2.points[:, 1], 'o')
-# # for simplex in p2.hull.simplices:
-# #     plt.plot(p2.points[simplex, 0], p2.points[simplex, 1], 'r-')
-#
-# # for simplex in p3.hull.simplices:
-# #     plt.plot(p3.points[simplex, 0], p3.points[simplex, 1], 'g-')
+p1 = MyPolygon(3)
+for i in range(3, 70):
+    p1.sides = i
+    print(p1.sides)
+    p1.generate_convex_hull_unit_circle(20)
+y = []
+for i in p1.areas.values():
+    y.append(np.mean(i))
+
+# make up some data in the interval ]0, 1[
+x = list(p1.areas.keys())
+# plot with various axes scales
+plt.figure(1)
+
+# linear
+plt.subplot(221)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.title('linear')
+plt.grid(True)
+
+# log
+plt.subplot(222)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.xscale('log')
+plt.title('log')
+plt.grid(True)
+# Format the minor tick labels of the y-axis into empty strings with
+# `NullFormatter`, to avoid cumbering the axis with too many labels.
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+plt.show()
--- NHull.py
+++ NHull.py
@@ -22,17 +22,20 @@ class NHull:
         self.polygon = Polygon(Polygon(self.points).convex_hull)
 
     def subscription(self, my_poly):
-        p = NHull(10)
-        p.polygon = self.polygon.intersection(my_poly.polygon)
-        x, y = p.polygon.exterior.coords.xy
-        my_points = np.empty([1, 2])
-        for i in range(len(x)):
-            my_points = np.append(my_points, [[x[i], y[i]]], 0)
+        p = NHull(self.vertex)
+        try:
+            p.polygon = self.polygon.intersection(my_poly.polygon)
+            x, y = p.polygon.exterior.coords.xy
+            my_points = np.empty([1, 2])
+            for i in range(len(x)):
+                my_points = np.append(my_points, [[x[i], y[i]]], 0)
 
-        my_points = np.delete(my_points, 0, 0)
-        new_hull = ConvexHull(my_points)
-        p.hull = new_hull
-        p.points = my_points
+            my_points = np.delete(my_points, 0, 0)
+            new_hull = ConvexHull(my_points)
+            p.hull = new_hull
+            p.points = my_points
+        except:
+            pass
         return p
 
 
@@ -45,37 +48,39 @@ def estimating_the_distribution(vertex, samples):
         p2.generate_convex_hull()
         p3 = p1.subscription(p2)
         # here for visualize and can removed for performance
-        plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
-        plt.plot(p2.points[:, 0], p2.points[:, 1], 'o')
-        for simplex in p1.hull.simplices:
-            plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'b-')
-        for simplex in p2.hull.simplices:
-            plt.plot(p2.points[simplex, 0], p2.points[simplex, 1], 'g-')
-        if not os.path.exists("static/NHull/sample" + str(i)):
-            os.makedirs("static/NHull/sample" + str(i))
-        plt.savefig("static/NHull/sample" + str(i) + "/points.jpg")
-        plt.close()
-        for simplex in p1.hull.simplices:
-            plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'g-')
-        for simplex in p2.hull.simplices:
-            plt.plot(p2.points[simplex, 0], p2.points[simplex, 1], 'r-')
-        for simplex in p3.hull.simplices:
-            plt.plot(p3.points[simplex, 0], p3.points[simplex, 1], 'b-')
-        plt.savefig("static/NHull/sample" + str(i) + "/Hull.jpg")
-        plt.close()
+        # plt.plot(p1.points[:, 0], p1.points[:, 1], 'o')
+        # plt.plot(p2.points[:, 0], p2.points[:, 1], 'o')
+        # for simplex in p1.hull.simplices:
+        #     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'b-')
+        # for simplex in p2.hull.simplices:
+        #     plt.plot(p2.points[simplex, 0], p2.points[simplex, 1], 'g-')
+        # if not os.path.exists("static/NHull/"+str(vertex)+"/sample" + str(i)):
+        #     os.makedirs("static/NHull/"+str(vertex)+"/sample" + str(i))
+        # plt.savefig("static/NHull/"+str(vertex)+"/sample" + str(i) + "/points.jpg")
+        # plt.close()
+        # for simplex in p1.hull.simplices:
+        #     plt.plot(p1.points[simplex, 0], p1.points[simplex, 1], 'g-')
+        # for simplex in p2.hull.simplices:
+        #     plt.plot(p2.points[simplex, 0], p2.points[simplex, 1], 'r-')
+        # if p3.polygon.area>0:
+        #     for simplex in p3.hull.simplices:
+        #         plt.plot(p3.points[simplex, 0], p3.points[simplex, 1], 'b-')
+        # plt.savefig("static/NHull/"+str(vertex)+"/sample" + str(i) + "/Hull.jpg")
+        # plt.close()
         data.append(p3.polygon.area)
-    y = [0 for i in range(200)]
+    y = [0 for i in range(100)]
     for i in data:
-        y[math.ceil(i / 0.005) - 1] += 1
-    y_pos = np.arange(200)
+        y[math.ceil(i / 0.01) - 1] += 1
+    y_pos = np.arange(100)
     performance = y
     plt.bar(y_pos, performance, align='center', alpha=1)
-    # plt.xticks(y_pos, objects)
-    # plt.show()
-    if not os.path.exists("static/NHull/bar_chart/"):
-        os.makedirs("static/NHull/bar_chart/")
-    plt.savefig("static/NHull/bar_chart/bar.jpg")
+    plt.xticks(y_pos, [])
+    if not os.path.exists("static/NHull/bar_charts/"):
+        os.makedirs("static/NHull/bar_charts/")
+    plt.savefig("static/NHull/bar_charts/bar" + str(vertex) + "jpg")
     print(data)
 
 
-estimating_the_distribution(4000, 400)
+my_vertexes = [5, 10, 15, 100, 200, 500, 4000]
+for j in my_vertexes:
+    estimating_the_distribution(j, 10)
